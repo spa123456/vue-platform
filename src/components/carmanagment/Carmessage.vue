0afdfcbb3f6653ab0270c6556790065bd841ad26
shipingan <740425014@qq.com>
2019-09-16 17:55:27 +0800

    fix:基本上差不多了 弹出框

--- src/components/carmanagment/Carmessage.vue
+++ src/components/carmanagment/Carmessage.vue
@@ -72,7 +72,7 @@
         </div>
         <div class="querydatelist-Bx">
           <div class="main-changeBx">
-            <el-select v-model="value" placeholder="请选择">
+            <el-select v-model="mainvalue" placeholder="请选择">
               <el-option
                 v-for="item in options"
                 :key="item.value"
@@ -80,6 +80,7 @@
                 :value="item.value"
               ></el-option>
             </el-select>
+            <span style="padding-left:20px;color:#aaa;font-size:14px;">下拉列表可选择子停车场进行设置</span>
           </div>
           <div class="querydatelist">
             <span>
@@ -92,7 +93,15 @@
                 </el-col>
                 <el-col :span="6">
                   <div class="grid-content">
-                    <el-switch v-model="statusone" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+                    <el-switch
+                      v-model="statusone"
+                      active-color="#13ce66"
+                      inactive-color="#ff4949"
+                      :active-text="switchtextone"
+                      active-value="100"
+                      inactive-value="0"
+                      @change="getvaluestatuone()"
+                    ></el-switch>
                   </div>
                 </el-col>
               </el-row>
@@ -106,7 +115,17 @@
                   </div>
                 </el-col>
                 <el-col :span="6">
-                  <div class="grid-content"></div>
+                  <div class="grid-content">
+                    <el-switch
+                      v-model="statustwo"
+                      active-color="#13ce66"
+                      inactive-color="#ff4949"
+                      :active-text="switchtexttwo"
+                      active-value="100"
+                      inactive-value="0"
+                      @change="getvaluestatutwo()"
+                    ></el-switch>
+                  </div>
                 </el-col>
               </el-row>
             </span>
@@ -119,7 +138,11 @@
                   </div>
                 </el-col>
                 <el-col :span="6">
-                  <div class="grid-content"></div>
+                  <div class="grid-content">
+                    <el-input v-model="settimem1" @blur="setparmeter1()">
+                      <template slot="append">分钟</template>
+                    </el-input>
+                  </div>
                 </el-col>
               </el-row>
             </span>
@@ -132,7 +155,17 @@
                   </div>
                 </el-col>
                 <el-col :span="6">
-                  <div class="grid-content"></div>
+                  <div class="grid-content">
+                    <el-switch
+                      v-model="statusthree"
+                      active-color="#13ce66"
+                      inactive-color="#ff4949"
+                      :active-text="switchtextthree"
+                      active-value="100"
+                      inactive-value="0"
+                      @change="getvaluestatuthree()"
+                    ></el-switch>
+                  </div>
                 </el-col>
               </el-row>
             </span>
@@ -145,7 +178,17 @@
                   </div>
                 </el-col>
                 <el-col :span="6">
-                  <div class="grid-content"></div>
+                  <div class="grid-content">
+                    <el-switch
+                      v-model="statusfour"
+                      active-color="#13ce66"
+                      inactive-color="#ff4949"
+                      :active-text="switchtextfour"
+                      active-value="100"
+                      inactive-value="0"
+                      @change="getvaluestatufour()"
+                    ></el-switch>
+                  </div>
                 </el-col>
               </el-row>
             </span>
@@ -158,7 +201,11 @@
                   </div>
                 </el-col>
                 <el-col :span="6">
-                  <div class="grid-content"></div>
+                  <div class="grid-content">
+                    <el-input v-model="settimeday" @blur="setparmeter2()">
+                      <template slot="append">天数</template>
+                    </el-input>
+                  </div>
                 </el-col>
               </el-row>
             </span>
@@ -171,7 +218,11 @@
                   </div>
                 </el-col>
                 <el-col :span="6">
-                  <div class="grid-content"></div>
+                  <div class="grid-content">
+                    <el-input v-model="settimem2" @blur="setparmeter3()">
+                      <template slot="append">分钟</template>
+                    </el-input>
+                  </div>
                 </el-col>
               </el-row>
             </span>
@@ -184,7 +235,11 @@
                   </div>
                 </el-col>
                 <el-col :span="6">
-                  <div class="grid-content"></div>
+                  <div class="grid-content">
+                    <el-input v-model="settimen1" @blur="setparmeter4()">
+                      <template slot="append">个数</template>
+                    </el-input>
+                  </div>
                 </el-col>
               </el-row>
             </span>
@@ -197,14 +252,216 @@
                   </div>
                 </el-col>
                 <el-col :span="6">
-                  <div class="grid-content"></div>
+                  <div class="grid-content">
+                    <el-input v-model="settimen2" @blur="setparmeter5()">
+                      <template slot="append">个数</template>
+                    </el-input>
+                  </div>
                 </el-col>
               </el-row>
             </span>
           </div>
         </div>
       </el-main>
-      <el-footer class="main-footer" height="130px">Footer</el-footer>
+      <el-footer class="main-footer" height="130px">
+        <div>
+          <h4>管理设置</h4>
+        </div>
+        <div class="footer-changeBx">
+          <el-select v-model="footervalue" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+          <span style="padding-left:20px;color:#aaa;font-size:14px;">下拉列表可选择子停车场进行设置</span>
+
+          <!-- 对话框页面 -->
+          <el-button type="primary" class="addbtn" @click="addparmeter = true">添加</el-button>
+          <!-- ///// -->
+          <el-dialog title="计费规则设置" :visible.sync="addparmeter">
+            <span class="dialog-span">
+              <el-row>
+                <el-col :span="18">
+                  <div>
+                    <h5>规则名称</h5>
+                    <h6>设定计费规则名称。</h6>
+                  </div>
+                </el-col>
+                <el-col :span="6">
+                  <div class="grid-content">
+                    <el-input v-model="settimem1" @blur="setparmeter1()"></el-input>
+                  </div>
+                </el-col>
+              </el-row>
+            </span>
+            <span class="dialog-span">
+              <el-row>
+                <el-col :span="18">
+                  <div>
+                    <h5>停车分类</h5>
+                    <h6>设定车辆分类，实现不同车主区分计费。</h6>
+                  </div>
+                </el-col>
+                <el-col :span="6">
+                  <div class="grid-content">
+                    <el-select v-model="typevalue" placeholder="请选择">
+                      <el-option
+                        v-for="item in typeoptions"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                      ></el-option>
+                    </el-select>
+                  </div>
+                </el-col>
+              </el-row>
+            </span>
+            <span class="dialog-span">
+              <el-row>
+                <el-col :span="18">
+                  <div>
+                    <h5>车牌颜色</h5>
+                    <h6>设定车牌颜色，按不同车牌颜色区分计费。</h6>
+                  </div>
+                </el-col>
+                <el-col :span="6">
+                  <div class="grid-content">
+                    <el-select v-model="colorvalue" placeholder="请选择">
+                      <el-option
+                        v-for="item in coloroptions"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                      ></el-option>
+                    </el-select>
+                  </div>
+                </el-col>
+              </el-row>
+            </span>
+            <span class="dialog-span">
+              <el-row>
+                <el-col :span="18">
+                  <div>
+                    <h5>车型类别</h5>
+                    <h6>设定车型，按车辆类型区分计费。</h6>
+                  </div>
+                </el-col>
+                <el-col :span="6">
+                  <div class="grid-content">
+                    <el-select v-model="cartypevalue" placeholder="请选择">
+                      <el-option
+                        v-for="item in cartypeoptions"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                      ></el-option>
+                    </el-select>
+                  </div>
+                </el-col>
+              </el-row>
+            </span>
+            <span class="dialog-span">
+              <el-row>
+                <el-col :span="18">
+                  <div>
+                    <h5>无入场默认金额</h5>
+                    <h6>无入场记录时，车辆默认收费金额。</h6>
+                  </div>
+                </el-col>
+                <el-col :span="6">
+                  <div class="grid-content">
+                    <el-input v-model="settimem1" @blur="setparmeter1()">
+                      <template slot="append">元</template>
+                    </el-input>
+                  </div>
+                </el-col>
+              </el-row>
+            </span>
+            <span class="dialog-span">
+              <el-row>
+                <el-col :span="18">
+                  <div>
+                    <h5>价格设置</h5>
+                    <h6>设置具体价格，系统支持分段计费哟。</h6>
+                  </div>
+                </el-col>
+                <el-col :span="6">
+                  <div class="grid-content">
+                    <el-row class="grid-content-row">
+                      <el-col :span="12">
+                        <el-select v-model="computervalue" placeholder="请选择">
+                          <el-option
+                            v-for="item in computeroptions"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                          ></el-option>
+                        </el-select>
+                      </el-col>
+                      <el-col :span="12">
+                        <el-select v-model="computertimevalue" placeholder="请选择">
+                          <el-option
+                            v-for="item in computertimeoptions"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                          ></el-option>
+                        </el-select>
+                      </el-col>
+                    </el-row>
+                  </div>
+                </el-col>
+                <div class="dialog-setmoney">
+                  <el-row>
+                    <el-col :span="4">免费时间：</el-col>
+                    <el-col :span="20">
+                      <el-input placeholder="请输入时间"></el-input>分钟
+                    </el-col>
+                  </el-row>
+                  <el-row>
+                    <el-col :span="4">起步价：</el-col>
+                    <el-col :span="20">
+                      <el-input></el-input>元
+                      <el-input></el-input>小时
+                    </el-col>
+                  </el-row>
+                  <el-row>
+                    <el-col :span="4">后续价：</el-col>
+                    <el-col :span="20">
+                      <el-input></el-input>元
+                      <el-input></el-input>小时
+                    </el-col>
+                  </el-row>
+                </div>
+              </el-row>
+            </span>
+            <span class="dialog-span">
+              <el-row>
+                <el-col :span="18">
+                  <div>
+                    <h5>支付完成后，离场限制</h5>
+                    <h6>设置电子支付后，离场的时间限制。</h6>
+                  </div>
+                </el-col>
+                <el-col :span="6">
+                  <div class="grid-content">
+                    <el-input v-model="settimem1" @blur="setparmeter1()">
+                      <template slot="append">分钟</template>
+                    </el-input>
+                  </div>
+                </el-col>
+              </el-row>
+            </span>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="addparmeter = false">取 消</el-button>
+              <el-button type="primary" @click="addparmeter = false">确 定</el-button>
+            </div>
+          </el-dialog>
+        </div>
+      </el-footer>
     </el-container>
   </el-container>
 </template>
@@ -228,13 +485,165 @@ export default {
         }
       ],
       options: [],
-      value: "",
-      statusone:'',
+      typevalue: "0",
+      typeoptions: [
+        {
+          value: "0",
+          label: "临时停车"
+        },
+        {
+          value: "1",
+          label: "普通停车"
+        }
+      ], //设置对话框中的临时停车和普通停车
+      colorvalue: "0",
+      coloroptions: [
+        {
+          value: "0",
+          label: "不区分"
+        },
+        {
+          value: "1",
+          label: "蓝色"
+        },
+        {
+          value: "2",
+          label: "黄色"
+        },
+        {
+          value: "3",
+          label: "黑色"
+        },
+        {
+          value: "4",
+          label: "绿色"
+        }
+      ], //车牌的颜色分类
+      cartypevalue: "0",
+      cartypeoptions: [
+        {
+          value: "0",
+          label: "不区分"
+        },
+        {
+          value: "1",
+          label: "小心汽车"
+        },
+        {
+          value: "2",
+          label: "中型汽车"
+        },
+        {
+          value: "3",
+          label: "大型汽车"
+        },
+        {
+          value: "4",
+          label: "其他"
+        }
+      ], //车辆类型分类
+      computervalue: "0",
+      computeroptions: [
+        {
+          value: "0",
+          label: "统一计费"
+        },
+        {
+          value: "1",
+          label: "分段计费"
+        }
+      ], //计费方式分类===>统一/分段计费
+      computertimevalue: "0",
+      computertimeoptions: [
+        {
+          value: "0",
+          label: "按小时计"
+        },
+        {
+          value: "1",
+          label: "按分钟计"
+        }
+      ], //计费计时方式
+      mainvalue: "", //管理设置下拉绑定值
+      footervalue: "", //
+      statusone: "100", //开启关闭的状态码
+      statustwo: "0", //开启关闭的状态码
+      statusthree: "0", //开启关闭的状态码
+      statusfour: "100", //开启关闭的状态码
+      switchtextone: "开启",
+      switchtexttwo: "开启",
+      switchtextthree: "开启",
+      switchtextfour: "开启",
+      settimem1: "30", //设置电子支付后，离场的时间限制。
+      settimeday: "100", //设置允许包月超期天数。
+      settimem2: "1000", //设置包月车辆临停免费时长。
+      settimen1: "1", //设置剩余空车位数。
+      settimen2: "100", //设置停车场总车位数。
+      addparmeter: false
     };
   },
   methods: {
+    /*
+     **  @description 左侧导航table的点击事件
+     **  @param {}
+     **  @return
+     **  @author shipingan
+     */
     querydate(row) {
       console.log(row.date);
+    },
+
+    /*
+     **  @description switch开关文字的切换
+     **  @param {}
+     **  @return
+     **  @author shipingan
+     */
+    getvaluestatuone() {
+      this.statusone == 0
+        ? (this.switchtextone = "关闭")
+        : (this.switchtextone = "开启");
+    },
+    getvaluestatutwo() {
+      this.statustwo == 0
+        ? (this.switchtexttwo = "关闭")
+        : (this.switchtexttwo = "开启");
+    },
+    getvaluestatuthree() {
+      this.statusthree == 0
+        ? (this.switchtextthree = "关闭")
+        : (this.switchtextthree = "开启");
+    },
+    getvaluestatufour() {
+      this.statusfour == 0
+        ? (this.switchtextfour = "关闭")
+        : (this.switchtextfour = "开启");
+    },
+    /*
+     **  @description 修改离场的时间限制。
+     **  @param {}
+     **  @return
+     **  @author shipingan
+     */
+    setparmeter1() {
+      console.log(this.settimem1);
+      //此处有一个修改成功的弹出
+    },
+    setparmeter2() {
+      console.log(this.settimeday);
+      //此处有一个修改成功的弹出
+    },
+    setparmeter3() {
+      console.log(this.settimem2);
+      //此处有一个修改成功的弹出
+    },
+    setparmeter4() {
+      console.log(this.settimen1);
+      //此处有一个修改成功的弹出
+    },
+    setparmeter5() {
+      console.log(this.settimen2);
+      //此处有一个修改成功的弹出
     }
   }
 };
@@ -325,19 +734,58 @@ export default {
         flex-direction: column;
         justify-content: space-between;
         span {
+          border-bottom: 1px double rgb(243, 238, 238);
           h6 {
             font-size: 12px;
             color: rgb(161, 161, 161);
+          }
+          .grid-content {
+            text-align: right;
+            .el-input {
+              width: 150px;
+            }
           }
         }
       }
     }
   }
   .main-footer {
+    overflow: auto;
+    .footer-changeBx {
+      margin-top: 10px;
+      position: relative;
+    }
   }
   h4 {
     padding: 10px 0;
     border-bottom: 1px solid #ccc;
   }
+  .dialog-span {
+    .el-row {
+      padding: 5px;
+      border-bottom: 1px double #ccc;
+    }
+    .dialog-setmoney {
+      .el-input {
+        width: 150px;
+      }
+      .el-row {
+        border: none;
+        padding-left: 40px;
+      }
+    }
+    h6 {
+      color: #ccc;
+    }
+    .grid-content-row {
+      border: none;
+    }
+  }
+}
+</style>
+<style >
+.addbtn {
+  position: absolute;
+  right: 0px;
 }
 </style>
